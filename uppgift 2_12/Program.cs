ad2939ab1600891b1dbbfa94f4d1ee31084a2d13
Mahdiiii4 <111962604+Mahdiiii4@users.noreply.github.com>
2022-09-14 11:52:37 +0200

    Update Program.cs
    uppgift 2_12

--- uppgift 2_12/Program.cs
+++ uppgift 2_12/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace uppgift2_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej du kan hyra ett bill för 300 kronor som start avgift och 1kr för varje km Den kör?." +
+                " Den kostar 500 kr för extra dagar");
+            Console.WriteLine("Vill du hyra billen?");
+            Console.ReadLine();
+            Console.WriteLine("För hur mycket kilometer kommer du och hyra den?");
+            int km = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket dager kommer du och hyra den?");
+            int dag = int.Parse(Console.ReadLine());
+            int resultat = dag*500+km+300;
+            Console.WriteLine("Kostnaden är " + resultat + ".");
+        }
+    }
+}
